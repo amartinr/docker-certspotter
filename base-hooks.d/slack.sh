55e39f3bdbb16342ebe03f8da774b3c024bd5ade
Abel Martín <abel.martin.ruiz@gmail.com>
2023-11-23 23:14:18 +0100

    Remove more references to 'bash'

--- base-hooks.d/slack.sh
+++ base-hooks.d/slack.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 # shellcheck disable=SC1091
-source /certspotter/utils.bash
+source /certspotter/utils.sh
 
 if [ -n "${1}" ] && [ -n "${CS_SLACK_URL}" ]; then
   curl -X POST --silent --data-urlencode "payload={\"text\": \"$1\"}" "$CS_SLACK_URL";
